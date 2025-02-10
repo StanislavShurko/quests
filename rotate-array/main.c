84fc50796df8aeefc18f1be860d5426fb29ff3e6
StanislavShurko <stas.shurko@gmail.com>
2025-02-10 19:40:06 +0100

    Rotate array

--- rotate-array/main.c
+++ rotate-array/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 
-#define arrSize 7
+#define arrSize 2
 
-
-void rotate(int* nums, int numsSize, int k) {
-  int shifts = k % numsSize;
-
-  for (int i = 0; i < numsSize; i++) {
-
+void reverse(int* arr, int start, int end) {
+  while (start < end) {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
   }
 }
 
+void rotate(int* nums, int numsSize, int k) {
+  int shift = k % numsSize;
+
+  reverse(nums, 0, numsSize - 1);
+  reverse(nums, 0, shift - 1);
+  reverse(nums, shift, numsSize - 1);
+}
+
 int main() {
-  int nums[arrSize] = { 1, 2, 3, 4, 5, 6, 7 };
+  int nums[arrSize] = { 1, 2 };
   int k = 3;
 
   rotate(nums, arrSize, k);
